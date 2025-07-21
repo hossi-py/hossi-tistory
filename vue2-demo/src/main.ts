71253869f1f4192c7bb7aeedd1fed11bd51e0860
황윤호 <d220191@kbfg.com>
2025-07-21 10:00:51 +0900

    vue2-demo 프로젝트 재구성 및 vue-query 플러그인 등록

--- vue2-demo/src/main.ts
+++ vue2-demo/src/main.ts
@@ -1,10 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import VueCompositionAPI, { h } from '@vue/composition-api';
+import { VueQueryPlugin } from '@tanstack/vue-query';
+
+Vue.use(VueCompositionAPI);
+Vue.use(VueQueryPlugin);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: () => h(App),
+}).$mount('#app');
