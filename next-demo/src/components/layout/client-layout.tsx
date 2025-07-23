36d4ce22e0317adf50a4035987c1a377e2da6fa3
황윤호 <d220191@kbfg.com>
2025-07-23 17:57:00 +0900

    컨텐츠가 헤더 영역까지 오도록

--- next-demo/src/components/layout/client-layout.tsx
+++ next-demo/src/components/layout/client-layout.tsx
@@ -16,15 +16,13 @@ export default function ClientLayout({
   return (
     <main
       className={cn(
-        "transition-all duration-300 pt-[var(--height-header)]",
+        "transition-all duration-300",
         expanded
           ? "ml-[var(--width-expanded-lnb)]"
           : "ml-[var(--width-collapsed-lnb)]"
       )}
     >
-      <div className="px-20 max-w-full overflow-x-auto break-words">
-        {children}
-      </div>
+      <div className="px-30 max-w-full break-words">{children}</div>
     </main>
   );
 }
