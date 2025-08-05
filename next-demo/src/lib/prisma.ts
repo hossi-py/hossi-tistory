668c2869e8af15be1b3028840f98cb5534e530c3
황윤호 <d220191@kbfg.com>
2025-08-05 14:44:13 +0900

    prisma 기본 세팅

---
+++ next-demo/src/lib/prisma.ts
@@ -0,0 +1,17 @@
+/** App 전체에서 Prisma Client 인스턴스가 하나만 생성되도록
+ * Next.js 개발 모드에서 핫 리로딩 문제 해결
+ */
+
+import { PrismaClient } from "@/generated/prisma";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
