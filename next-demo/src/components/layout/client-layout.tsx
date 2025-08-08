9ee421696537846eb07be030afb2d78358447ff6
황윤호 <d220191@kbfg.com>
2025-08-08 17:05:18 +0900

    main 영역 css 수정

--- next-demo/src/components/layout/client-layout.tsx
+++ next-demo/src/components/layout/client-layout.tsx
@@ -19,15 +19,17 @@ export default function ClientLayout({
     <main
       className={cn(
         "pt-[var(--height-header)]",
-        "h-full flex flex-col",
+        "h-dvh flex flex-col",
         "transition-all duration-300",
-        isMobile ? "!m-0" : "",
-        expanded && !isMobile
-          ? "ml-[var(--width-expanded-lnb)]"
-          : "ml-[var(--width-collapsed-lnb)]"
+        isMobile
+          ? "ml-0 w-full"
+          : expanded
+          ? "ml-[var(--width-expanded-lnb)] w-[calc(100vw-var(--width-expanded-lnb))]"
+          : "ml-[var(--width-collapsed-lnb)] w-[calc(100vw-var(--width-collapsed-lnb))]",
+        "overflow-x-hidden"
       )}
     >
-      {children}
+      <div className="min-w-0 break-words">{children}</div>
     </main>
   );
 }
--- next-demo/src/components/navigation/gnb-mobile.tsx
+++ next-demo/src/components/navigation/gnb-mobile.tsx
@@ -8,7 +8,7 @@ export default function GNBMobile() {
   const setExpanded = useSidebarExpandedStore((s) => s.setExpanded);
 
   return (
-    <header className="fixed flex items-center p-2 w-full top-0 h-[var(--height-header)] border-b border-zinc-700 z-10">
+    <header className="fixed flex items-center p-2 w-full top-0 h-[var(--height-header)] border-b border-zinc-700 z-10 bg-background">
       <Button
         onClick={() => setExpanded(true)}
         className="cursor-pointer"
