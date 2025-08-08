cd6f779c763db83feefb70ac332ee7178f0dc010
황윤호 <d220191@kbfg.com>
2025-08-08 15:03:47 +0900

    github ssr 호출

--- next-demo/src/app/projects/page.tsx
+++ next-demo/src/app/projects/page.tsx
@@ -1,7 +1,14 @@
+import { githubFetch } from "@/lib/github";
+
 export default async function ProjectsPage() {
+  const user = await githubFetch("/user");
+
   return (
     <div>
-      <h1>Page</h1>
+      {JSON.stringify(user)}
+      {/* <h1>Projects</h1> */}
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      {/* <h1>Page</h1> */}
     </div>
   );
 }
